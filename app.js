53680dcb12d04ad7c23c11d27ba1adaa4e9e0673
FizzaChouhan <chouhanfizza@gmail.com>
2024-08-21 00:38:54 +0500

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+function addition() {
+   var list= document.getElementById("some")
+   var data = document.getElementById("userInput")
+   var li=document.createElement("li");
+   li.innerHTML=userInput.value
+  list.appendChild(li)
+  console.log(li)
+
+  listItem.style.color = "pink"; 
+   
+   
+}
